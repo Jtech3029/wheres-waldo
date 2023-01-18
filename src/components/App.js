bf07dd14cea9356fa8a50f9af547ec38a3c5b949
CrypticAgro <jtech3029@gmail.com>
2023-01-18 16:00:59 -0700

    cleaned up files and added some components

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import WaldoPage from "./WaldoPage";
+import "../styles/App.css"
+function App() {
+  return (
+    <div className="App">
+      <WaldoPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WaldoPage.js
@@ -0,0 +1,11 @@
+import "../styles/WaldoPage.css"
+
+export default function WaldoPage() {
+    function renderCircle() {
+
+    }
+
+    return (<div id="waldo-page">
+        <img src={require("../imgs/wheres-waldo.png")} id="waldo-image" alt="waldo"></img>
+    </div>)
+}
